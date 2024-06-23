fcad7e1384a5b41f5db6be935438bf754b6fb1ab
EddWebPro <eduardihtiarov408@gmail.com>
2024-06-23 22:39:22 +0500

    +++

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import InputComponents from './components/Input/InputComponents'
+import SVG from './components/Input/SVG'
+
 
 function App() {
 
   return (
     <>
     <div className='wrapper'>
-        <InputComponents mode='phone'/>
+      
+        <InputComponents mode='phone'/> 
         <InputComponents mode='name'/>
         <InputComponents mode='textarea'/>
         
